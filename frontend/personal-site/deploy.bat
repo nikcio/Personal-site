700541e03dc51b071b633ba37b61bb8e0098443b
Nikolaj Brask-Nielsen <nikolajgive@gmail.com>
2022-01-16 23:47:52 +0100

    chore: Updated deploy file

--- frontend/personal-site/deploy.bat
+++ frontend/personal-site/deploy.bat
@@ -1,3 +1,3 @@
 docker-compose build
-docker tag personal-site_website-fe registry.digitalocean.com/nikcio/personal-site-fe
+docker tag personal-site_personal-site_website-fe registry.digitalocean.com/nikcio/personal-site-fe
 docker push registry.digitalocean.com/nikcio/personal-site-fe
