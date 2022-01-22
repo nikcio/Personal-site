d81cece9c1a01c026d82f1c57243a6616fb18862
Nikolaj Brask-Nielsen <nikolajgive@gmail.com>
2022-01-22 16:54:40 +0100

    chore: Docker dev trial

--- frontend/personal-site/deploy.bat
+++ frontend/personal-site/deploy.bat
@@ -1,3 +1,3 @@
-docker-compose build
+docker-compose -f ./docker-compose-build.yml build
 docker tag personal-site_personal-site_website-fe registry.digitalocean.com/nikcio/personal-site-fe
 docker push registry.digitalocean.com/nikcio/personal-site-fe
